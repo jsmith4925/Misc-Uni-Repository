13698dd3dd91a0bfe61f43009b7526c166b9cd16
Spooky <41446415+Sp00ke@users.noreply.github.com>
2018-10-12 08:54:34 +0100

    COMPLETED: Lab1

--- labs/9am hell/lab1/MyScript.py
+++ labs/9am hell/lab1/MyScript.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Task 1
 x = 345
 w = 2
 b = 5
@@ -18,6 +19,7 @@ print(y)
 print("Hello World")
 f(x, w, b)
 
+# Task 2
 B = [5, 6, 7, 8]
 for i in reversed(B):
     print(i)
@@ -32,6 +34,7 @@ def equals100(x):
 
 equals100(100)
 
+# Task 1.2
 myDictionary = {
     'data_name': "myData",
     'data': np.random.randint(0, 5, (20, 3)),
@@ -41,12 +44,18 @@ print(myDictionary['data_name'])
 print(myDictionary['data'])
 print(myDictionary['labels'])
 
-
+# Task 1.3
 arrayOne = np.random.randint(1, 10, (2, 3))
 arrayTwo = np.random.randint(1, 10, (3, 4))
 result = arrayOne.dot(arrayTwo)
 print(result[:, 0])
 
-data = np.load("Iris_data.npy")
-#HELP
-print(data)
+# Task 1.4
+data = np.load("lab1\\Iris_data.npy")
+
+plt.scatter(data[:, 0], data[:, 1])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title("Scatterplot of 2D data")
+
+plt.show()
