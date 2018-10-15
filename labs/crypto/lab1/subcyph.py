2f97f91e6ef028ef2abfd847dc68615f4a510c94
Spooky <41446415+Sp00ke@users.noreply.github.com>
2018-10-15 14:40:00 +0100

    crypto lab1:task1

---
+++ labs/crypto/lab1/subcyph.py
@@ -0,0 +1,21 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def encoder(key):    
+    message = input("Message to encode: ")
+    code = []
+    for i in message:
+        if i.strip() and i in alphabet:
+            code.append(alphabet[(alphabet.index(i) + key) %26])
+        else:
+            code.append(i)
+    
+    output = ''.join(code)
+    return output
+
+def main():
+    key = 0
+    key = int(input("Key(int 1-26): "))
+
+    print(encoder(key))
+
+main()
