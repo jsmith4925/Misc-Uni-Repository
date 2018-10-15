ce83ddfbfa31ed9607cb73fb4444689bc18c3d26
James Smith <jsmith4925@gmail.com>
2018-10-15 18:54:08 +0100

    crypto:lab1 added decoder

--- labs/crypto/lab1/subcyph.py
+++ labs/crypto/lab1/subcyph.py
@@ -12,10 +12,23 @@ def encoder(key):
     output = ''.join(code)
     return output
 
+def decode(key):    
+    message = input("Message to decode: ")
+    code = []
+    for i in message:
+        if i.strip() and i in alphabet:
+            code.append(alphabet[(alphabet.index(i) - key) %26])
+        else:
+            code.append(i)
+    
+    output = ''.join(code)
+    return output
+
 def main():
     key = 0
     key = int(input("Key(int 1-26): "))
 
     print(encoder(key))
+    print(decode(key))
 
 main()
