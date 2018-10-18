db5897c507e5c68445b82be9a0459bbb8063b192
James Smith <jsmith4925@gmail.com>
2018-10-18 01:36:30 +0100

    new side project: RPS. looking into enums

---
+++ side/Rps/rockpaperscissors.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+enum Moves { rock, paper, scissors }; //WHAT NOW? HOW DO I USE THIS MAGIC?
+
+int computerChoice() {
+    /* random int between 0 and 2 */
+    int r = rand() % 3;
+
+
+}
+
+int main() {
+    
+    std::cout << "Hello, World!";
+    return 0;
+}
