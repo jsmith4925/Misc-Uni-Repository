afae9aed9e70a5e8d92acbd4e1dc47f518bedbd6
Spooky <41446415+Sp00ke@users.noreply.github.com>
2018-10-18 13:01:28 +0100

    crypto: signed off lab1

--- labs/crypto/lab1/subcyph.py
+++ labs/crypto/lab1/subcyph.py
@@ -1,34 +1,37 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-def encoder(key):    
-    message = input("Message to encode: ")
+
+def encoder(key):
+    message = input("Message to encode: ").lower()
     code = []
     for i in message:
         if i.strip() and i in alphabet:
-            code.append(alphabet[(alphabet.index(i) + key) %26])
+            code.append(alphabet[(alphabet.index(i) + key) % 26])
         else:
             code.append(i)
-    
+
     output = ''.join(code)
     return output
 
-def decode(key):    
-    message = input("Message to decode: ")
+
+def decode(key):
+    message = input("Message to decode: ").lower()
     code = []
     for i in message:
         if i.strip() and i in alphabet:
-            code.append(alphabet[(alphabet.index(i) - key) %26])
+            code.append(alphabet[(alphabet.index(i) - key) % 26])
         else:
             code.append(i)
-    
+
     output = ''.join(code)
     return output
+
 
 def main():
     key = 0
     key = int(input("Key(int 1-26): "))
+    print(encoder(key).upper())
+    print(decode(key).lower())
 
-    print(encoder(key))
-    print(decode(key))
 
 main()
