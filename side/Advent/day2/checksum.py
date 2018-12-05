8166f4524545739a45a70d9b9acf7eb207453fe4
James Smith <jsmith4925@gmail.com>
2018-12-05 23:01:57 +0000

    advent of code day 2 part 1

--- side/Advent/day2/checksum.py
+++ side/Advent/day2/checksum.py
@@ -1,8 +1,23 @@
 import pandas as pd
+from collections import Counter
+
+chars = "abcdefghijklmnopqrstuvwxyz"
+
+twos = 0
+threes = 0
 
 with open("Advent\day2\\box_list.txt") as temp_file:
     labels = [line.strip('\n') for line in temp_file]
-
-for line in labels[0:3]:
-    print(pd.Series(line).value_counts())
-     
+for i in labels:
+    added_two = False
+    added_three = False
+    for char in chars:
+        count = i.count(char)
+        if count > 1:
+            if count == 3 and not added_three:
+                threes += 1
+                added_three = True
+            elif count == 2 and not added_two:
+                twos += 1
+                added_two = True
+print(twos * threes)
